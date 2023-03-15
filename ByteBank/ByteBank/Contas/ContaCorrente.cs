1dc71be1a70f9a51e8bb5fe9743ad7f221381bbc
André Evaristo Tavares <evaristoandre106@gmail.com>
2023-03-15 08:50:47 -0300

    Add files via upload

---
+++ ByteBank/ByteBank/Contas/ContaCorrente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using ByteBank.Titular;
+using ByteBank.Contas;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Contas
+{
+    public class ContaCorrente
+    {
+        public string Agencia;
+        public string Conta;
+        public Cliente Titular;
+        public double Saldo;
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+        public bool Saque(double valor)
+        {
+            if (Saldo > valor)
+            {
+                Saldo -= valor;
+                return true;
+            }
+            else { return false; }
+        }
+        public void DadosDaConta()
+        {
+            Console.WriteLine($"Agencia {Agencia}\nConta: {Conta}\nTitular: {Titular.Nome}\nSaldo: {Saldo.ToString("F2")}");
+        }
+        public bool Transferir(double valor, ContaCorrente contaCorrente)
+        {
+            if (Saldo > valor)
+            {
+                contaCorrente.Saldo += valor;
+                return true;
+            }
+            else { return false; }
+
+        }
+
+
+    }
+}
---
+++ ByteBank/ByteBank/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using ByteBank.Contas;
+using ByteBank.Titular;
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente();
+            cliente.Nome = "Andre";
+            cliente.Cpf = "22877086852";
+            cliente.Profissao = "Programador";
+
+            ContaCorrente contaCorrente = new ContaCorrente();
+            contaCorrente.Titular = cliente;
+            contaCorrente.Agencia = "1234";
+            contaCorrente.Conta = "1234-5";
+            contaCorrente.Saldo = 2000;
+
+
+            Console.WriteLine("Titular: "+contaCorrente.Titular.Nome);
+            Console.WriteLine("CPF: "+contaCorrente.Titular.Cpf);
+            Console.WriteLine("Profissao: "+contaCorrente.Titular.Profissao);
+            Console.WriteLine("Numero da conta:"+contaCorrente.Conta);
+            Console.WriteLine("Numero da agencia: " + contaCorrente.Agencia);
+            Console.WriteLine("Saldo: " + contaCorrente.Saldo);
+        }
+    }
+}
